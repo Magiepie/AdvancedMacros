e6084616821b0efeeb140a6d492e6927dcf2d714
Magiepie <iamthedustin@gmail.com>
2020-07-07 00:02:42 -0400

    OreQuarryLocations.lua

--- Mscape/mining.lua
+++ Mscape/mining.lua
@@ -1,19 +1,25 @@
-function ProspectBlock()
-	local IdToOre = {
-					["minecraft:nether_quartz_ore"] = "Copper" ,
-					["minecraft:iron_ore"]          = "Tin"    ,
-					["minecraft:emerald_ore"]       = "Iron"   ,
-					["minecraft:coal_ore"]          = "Coal"   ,
-					["minecraft:prismarine_bricks"] = "Silver" ,
-					["minecraft:gold_ore"]          = "Gold"   ,
-					["minecraft:redstone_ore"]      = "Mithril",
-					["minecraft:lapis_ore"]         = "Adamant",
-					["minecraft:bedrock"]           = "Depleted"}
-					
+local IdToOre = {
+                ["minecraft:nether_quartz_ore"] = "Copper" ,
+                ["minecraft:iron_ore"]          = "Tin"    ,
+                ["minecraft:emerald_ore"]       = "Iron"   ,
+                ["minecraft:coal_ore"]          = "Coal"   ,
+                ["minecraft:prismarine_bricks"] = "Silver" ,
+                ["minecraft:gold_ore"]          = "Gold"   ,
+                ["minecraft:redstone_ore"]      = "Mithril",
+                ["minecraft:lapis_ore"]         = "Adamant",
+                ["minecraft:bedrock"]           = "Depleted"}
+
+function ProspectBlockTarget()					
 	if getPlayer().target and getPlayer().target.block then
 		local prospectedOre = IdToOre[getPlayer().target.block.id]
 			if prospectedOre == nil then return "unidentified "..getPlayer().target.block.id end
 			return prospectedOre
 	end
 	return false
+end
+
+function ProspectBlock(x,y,z)
+    local prospectedOre = IdToOre[getBlock(x,y,z).id]
+    if prospectedOre == nil then return "unidentified "..getBlock(x,y,z).id end
+ return prospectedOre
 end
