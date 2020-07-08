53a13cb3a3e44f7f17fbb3399cd65a23e468fc34
Magiepie <iamthedustin@gmail.com>
2020-07-08 01:30:33 -0400

    Update walker.lua

--- Mscape/walker.lua
+++ Mscape/walker.lua
@@ -22,11 +22,25 @@ function MoveToXYZ(x,y,z)
 	forward(0)
 end
 
-function FollowPath(waypoints) --"lumbyAkBank"
+function GetWaypointStart(waypoints)
 	local path = GetWayPoints(waypoints)
 	local X,Y,Z = getPlayerBlockPos()
 	local distance1 = PlayerDist(X, Y, Z ,path[1].x,path[1].y,path[1].z)
 	local distance2 = PlayerDist(X, Y, Z ,path[#path].x,#path].y,#path].z)
-	
+	local var,limit, step
+	if distance1 > distance2 then
+		var = 1 limit = #path step = 1
+		return var, limit, step
+	else
+		var = #path limit = 1 step = -1
+		return var, limit, step
+	end
+end
+
+function FollowPath(waypoints) --"lumbyAkBank"
+	local var,limit, step = GetWaypointStart(waypoints)
+	for i = var ,limit, step do
+	print(i) 
+	end 
 end
 
